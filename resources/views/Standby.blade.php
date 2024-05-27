e82280af218830794349427e91032deb50857f2a
Your Name <you@example.com>
2024-05-27 15:32:59 +0700

    perbaikan

--- resources/views/Standby.blade.php
+++ resources/views/Standby.blade.php
@@ -31,7 +31,7 @@
                         <a class="nav-link" href="{{ route('login') }}">Login</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Sign Up</a>
+                        <a class="nav-link" href="{{ route('signup') }}">Sign Up</a>
                     </li>
                 </ul>
             </div>
