9f76dd27e6259d82b4cdd1671dda534b1e227786
Your Name <you@example.com>
2024-05-27 21:22:26 +0700

    pembaruan exp_db

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -82,28 +82,33 @@
         <h1 class="mb-4">Telusuri Berdasarkan Kategori</h1>
         <div class="scroll-container w-100 d-flex gap-4 mb-1">
             <a href="#" class="text-decoration-none text-dark">
-                <img src="{{ asset('/img/tempe_standby.png') }}" class="card-img-top" alt="Kategori 1">
+                <img src="{{ asset('/img/Kategori/KG_jakarta.png') }}" class="card-img-top" alt="Kategori 1">
             </a>
             <a href="#" class="text-decoration-none text-dark">
-                <img src="{{ asset('/img/tempe_standby.png') }}" class="card-img-top" alt="Kategori 2">
+                <img src="{{ asset('/img/Kategori/KG_jabar.png') }}" class="card-img-top" alt="Kategori 2">
             </a>
             <a href="#" class="text-decoration-none text-dark">
-                <img src="{{ asset('/img/tempe_standby.png') }}" class="card-img-top" alt="Kategori 3">
+                <img src="{{ asset('/img/Kategori/KG_jateng.png') }}" class="card-img-top" alt="Kategori 3">
             </a>
             <a href="#" class="text-decoration-none text-dark">
-                <img src="{{ asset('/img/tempe_standby.png') }}" class="card-img-top" alt="Kategori 4">
+                <img src="{{ asset('/img/Kategori/KG_banten.png') }}" class="card-img-top" alt="Kategori 4">
             </a>
             <a href="#" class="text-decoration-none text-dark">
-                <img src="{{ asset('/img/tempe_standby.png') }}" class="card-img-top" alt="Kategori 5">
+                <img src="{{ asset('/img/Kategori/KG_jatim.png') }}" class="card-img-top" alt="Kategori 5">
             </a>
             <a href="#" class="text-decoration-none text-dark">
-                <img src="{{ asset('/img/tempe_standby.png') }}" class="card-img-top" alt="Kategori 6">
+                <img src="{{ asset('/img/Kategori/KG_jogja.png') }}" class="card-img-top" alt="Kategori 6">
             </a>
         </div>
     </section>
 
     <!-- Footer -->
-    <footer class="footer mt-auto py-3">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#343a40" fill-opacity="1"
+            d="M0,128L48,144C96,160,192,192,288,213.3C384,235,480,245,576,213.3C672,181,768,107,864,106.7C960,107,1056,181,1152,176C1248,171,1344,85,1392,42.7L1440,0L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
+        </path>
+    </svg>
+    <footer class="footer mt-auto py-3 ">
         <div class="container">
             <div class="row">
                 <div class="col-md-4">
