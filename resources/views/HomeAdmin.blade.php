d35b374d44a7f29ff4b171f38bb9e2ce948cf6c5
Your Name <you@example.com>
2024-06-21 08:40:09 +0700

    perbaikan terakhir

--- resources/views/HomeAdmin.blade.php
+++ resources/views/HomeAdmin.blade.php
@@ -147,16 +147,10 @@
                 <div class="offcanvas-body">
                     <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('favorite')  }}">Favorite</a>
+                            <a class="nav-link active" aria-current="page" href="{{ route('homeadmin') }}">Home</a>
                         </li>
                         <li>
                             <a class="nav-link" href="{{ route('tambahmenu') }}">Tambahkan menu kreasimu</a>
-                        </li>
-                        <li>
-                            <a class="nav-link" href="{{ route('home') }}">Lihat daftar resep</a>
                         </li>
                         <li>
                             <form action="{{ route('logout') }}" method="POST">
